3612daa773f463fe415d12205bbd3356202e5a3e
Miroslav <miroslav.kartashov@gmail.com>
2020-08-05 15:44:37 +0300

    недоделка

---
+++ src/main/java/Cart.java
@@ -0,0 +1,60 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+@WebServlet(name = "CartServlet", urlPatterns = "/cart_servlet")
+public class Cart extends HttpServlet implements Servlet {
+
+    private static Logger logger = LoggerFactory.getLogger(Cart.class);
+
+    private transient ServletConfig config;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        logger.info("New request");
+        res.setContentType( "text/html" );
+        String text = "<!DOCTYPE html PUBLIC " +
+                "\"-//W3C//DTD HTML 4.01 Transitional//EN\" " +
+                "\"http://www.w3.org/TR/html4/loose.dtd\"> " +
+                "<html><head>" +
+                "<meta http-equiv=\"Content-Type\" " +
+                "content=\"text/html; charset=UTF-8\"> " +
+                "<title>Меню</title>" +
+                "</head>" +
+                "<body>" +
+                "<h1> Ваши покупки  Карзина </h1>" +
+                "</body></html>";
+        text = String.format(text, config.getInitParameter("Cart"),
+                config.getInitParameter("Cart"));
+        OutputStream outStream = res.getOutputStream();
+        outStream.write(text.getBytes("UTF-8"));
+        outStream.flush();
+        outStream.close();
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "CartServlet";
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("Servlet {} destroyed", getServletInfo());
+    }
+}
---
+++ src/main/java/Order.java
@@ -0,0 +1,64 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+@WebServlet(name = "OrderServlet", urlPatterns = "/order_servlet")
+public class Order extends HttpServlet implements Servlet {
+
+    private static Logger logger = LoggerFactory.getLogger(Order.class);
+
+    private transient ServletConfig config;
+
+    // Метод вызывается контейнером после того как был создан класс сервлета
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // Сохраняем полученную от сервера конфигурацию
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    // Метод вызывается для каждого нового HTTP запроса к данному сервлету
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        logger.info("New request");
+        res.setContentType( "text/html" );
+        String text = "<!DOCTYPE html PUBLIC " +
+                "\"-//W3C//DTD HTML 4.01 Transitional//EN\" " +
+                "\"http://www.w3.org/TR/html4/loose.dtd\"> " +
+                "<html><head>" +
+                "<meta http-equiv=\"Content-Type\" " +
+                "content=\"text/html; charset=UTF-8\"> " +
+                "<title>Меню</title>" +
+                "</head>" +
+                "<body>" +
+                "<h1> Ваши Заказы </h1>" +
+                "</body></html>";
+        text = String.format(text, config.getInitParameter("Заказы"),
+                config.getInitParameter("Заказы"));
+        OutputStream outStream = res.getOutputStream();
+        outStream.write(text.getBytes("UTF-8"));
+        outStream.flush();
+        outStream.close();
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "OrderServlet";
+    }
+
+    // При завершении работы веб приложения, контейнер вызывает этот метод для всех сервлетов из этого приложения
+    @Override
+    public void destroy() {
+        logger.info("Servlet {} destroyed", getServletInfo());
+    }
+}
